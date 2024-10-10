4a33574f677ca7b2bf2bbc0ee1155446c45f2699
rori0022 <s445003909@uqu.edu.sa>
2024-10-11 00:41:45 +0300

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q2;
+
+/**
+ *
+ * @author Rori
+ */
+public class Q2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
